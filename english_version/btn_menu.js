0d622462e64e97251d73d6dd1f4f2c60cbe29830
agaribaldi82 <adgaribaldi@gmail.com>
2024-04-29 00:20:20 -0300

    Menu funcionando, hay que agregar boton de cerrar al nav

--- english_version/btn_menu.js
+++ english_version/btn_menu.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector("#lista_contenedor")
 const abrir = document.querySelector("#menu_hamburguesa")
+const cerrar = document.querySelector("cerrar")
 
 abrir.addEventListener("click", () => {
     lista_contenedor.classList.add("visible")
+})
+
+cerrar.addEventListener("click", () => {
+    lista_contendor.classList.add("hide")
 })
